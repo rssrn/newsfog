9c3b33ff76d3cdc020b6372da6b657316ee81349
Ross Arnold <rossarn@gmail.com>
2016-04-30 21:32:25 +1200

    pre-commit hook for unit testing

---
+++ pre-commit.sh
@@ -0,0 +1,25 @@
+echo "running pre-commit.sh"
+
+#RESULT=$?
+#if [ $RESULT -ne 0 ]; then
+#	echo "FAIL"
+#else
+#	echo "PASS"
+#fi
+#exit 1
+
+if git diff-index --quiet HEAD --; then
+	# no changes between index and working copy; just run tests
+	./node_modules/.bin/mocha --reporter dot
+	RESULT=$?
+else
+	# Test the version that's about to be committed,
+	# stashing all unindexed changes
+	git stash -q --keep-index
+	./node_modules/.bin/mocha --reporter dot
+	RESULT=$?
+	git stash pop -q
+fi
+
+[ $RESULT -ne 0 ] && exit 1
+exit 0
