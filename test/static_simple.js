8e4edb1d51887958846b447a7ec48eddac02cbd6
Ross Arnold <rossarn@gmail.com>
2016-05-05 21:44:49 +1200

    add simple async tests

---
+++ test/static_simple.js
@@ -0,0 +1,52 @@
+var expect  = require("chai").expect;
+
+var expr = require('../app/server');
+
+http = require('http');
+
+var app, server, port;
+
+// TODO consider making the before and after into globals
+// for easy re-use with other tests
+
+before(function () {
+	app = expr();
+	// don't specif the port - so it will pick a random port
+	// (to reduce chance of conflicting with other tests running in parallel)
+	server = app.listen(function() {
+		port = server.address().port;
+	});
+});
+
+describe('/test/small_static_test', function () {
+	it('should return 200', function (done) {
+		http.get('http://localhost:' + port, function (res) {
+			expect(res.statusCode).to.equal(200);
+			done();
+		});
+	});
+	
+	it('should say "hello"', function (done) {
+		http.get(
+			'http://localhost:'
+			+ port
+			+ '/test/small_static_test.txt',
+		function (res) {
+			var data = '';
+			
+			res.on('data', function (chunk) {
+				data += chunk;
+			});
+			
+			res.on('end', function () {
+				expect(data).to.equal('hello\n');
+				done();
+			});
+		});
+	});
+});
+
+
+after(function () {
+	server.close();
+});
